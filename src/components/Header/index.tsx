63b0b1d29d8596335fa716485b26b09d12b64ea4
lizhiheng <lizhiheng@kidcodes.cn>
2023-07-17 20:29:45 +0800

    feat(logo): 调整logo
    1.删除不要的图片
    2.修改不必要的链接

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,18 @@
-import {BoldOutlined, GithubOutlined} from '@ant-design/icons';
-import {Button, Modal, Space} from 'antd';
-import {useResponsive} from 'antd-style';
+import { BoldOutlined, LinkOutlined } from '@ant-design/icons';
+import { Button, Modal, Space } from 'antd';
+import { useResponsive } from 'antd-style';
 import qs from 'query-string';
-import {type ReactNode, memo, useCallback, useEffect, useState} from 'react';
+import { type ReactNode, memo, useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import {shallow} from 'zustand/shallow';
+import { shallow } from 'zustand/shallow';
 
-import {DraggablePanel} from '@/components';
-import {useAppStore} from '@/store';
+import { DraggablePanel } from '@/components';
+import { useAppStore } from '@/store';
 
-import Giscus from './Giscus';
 import Logo from './Logo';
 import Nav from './Nav';
 import Setting from './Setting';
-import {civitaiLogo, themeIcon} from './style';
+import { civitaiLogo, themeIcon } from './style';
 
 /******************************************************
  *********************** Style *************************
@@ -38,79 +37,73 @@ interface HeaderProps {
   children: ReactNode;
 }
 
-const Header = memo<HeaderProps>(({children}) => {
-    const [themeMode] = useAppStore((st) => [st.themeMode], shallow);
-    const {mobile} = useResponsive();
-    const [expand, setExpand] = useState<boolean>(true);
-    const [isModalOpen, setIsModalOpen] = useState(false);
+const Header = memo<HeaderProps>(({ children }) => {
+  const [themeMode] = useAppStore((st) => [st.themeMode], shallow);
+  const { mobile } = useResponsive();
+  const [expand, setExpand] = useState<boolean>(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    useEffect(() => {
-        if (mobile) setExpand(false);
-    }, []);
+  useEffect(() => {
+    if (mobile) setExpand(false);
+  }, []);
 
-    const handleSetTheme = useCallback(() => {
-        const theme = themeMode === 'light' ? 'dark' : 'light';
-        const gradioURL = qs.parseUrl(window.location.href);
-        gradioURL.query.__theme = theme;
-        window.location.replace(qs.stringifyUrl(gradioURL));
-    }, [themeMode]);
+  const handleSetTheme = useCallback(() => {
+    const theme = themeMode === 'light' ? 'dark' : 'light';
+    const gradioURL = qs.parseUrl(window.location.href);
+    gradioURL.query.__theme = theme;
+    window.location.replace(qs.stringifyUrl(gradioURL));
+  }, [themeMode]);
 
-    const showModal = () => setIsModalOpen(true);
+  const showModal = () => setIsModalOpen(true);
 
-    const handleCancel = () => setIsModalOpen(false);
+  const handleCancel = () => setIsModalOpen(false);
 
-    return (
-        <>
-            <DraggablePanel
-                defaultSize={{height: 'auto'}}
-                expand={expand}
-                minHeight={64}
-                onExpandChange={setExpand}
-                placement="top"
-            >
-                <HeaderView id="header" style={{flexDirection: mobile ? 'column' : 'row'}}>
+  return (
+    <>
+      <DraggablePanel
+        defaultSize={{ height: 'auto' }}
+        expand={expand}
+        minHeight={64}
+        onExpandChange={setExpand}
+        placement="top"
+      >
+        <HeaderView id="header" style={{ flexDirection: mobile ? 'column' : 'row' }}>
+          <a href="https://www.vedegpt.com" rel="noreferrer" target="_blank">
+            <Logo themeMode={themeMode} />
+          </a>
+          <Nav />
+          {children}
 
-                    <Nav />
-                    {children}
-
-                    <Space.Compact>
-                        <a href="https://civitai.com/" rel="noreferrer" target="_blank">
-                            <Button icon={civitaiLogo} title="Civitai" />
-                        </a>
-                        <a
-                            href="https://www.birme.net/?target_width=512&target_height=512"
-                            rel="noreferrer"
-                            target="_blank"
-                        >
-                            <Button icon={<BoldOutlined />} title="Birme" />
-                        </a>
-                        <Button icon={<GithubOutlined />} onClick={showModal} title="Feedback" />
-                        <Setting />
-                        <Button icon={themeIcon[themeMode]} onClick={handleSetTheme} title="Switch Theme" />
-                    </Space.Compact>
-                </HeaderView>
-            </DraggablePanel>
-            <Modal
-                footer={false}
-                onCancel={handleCancel}
-                open={isModalOpen}
-                title={
-                    <a
-                        href="https://github.com/canisminor1990/sd-webui-kitchen-theme"
-                        rel="noreferrer"
-                        target="_blank"
-                    >
-                        <Space>
-                            <GithubOutlined />
-                            {'canisminor1990/sd-webui-kitchen-theme'}
-                        </Space>
-                    </a>
-                }
-            >
-                <Giscus themeMode={themeMode} />
-            </Modal>
-        </>
-    );
+          <Space.Compact>
+            <a href="https://models.vedegpt.com/" rel="noreferrer" target="_blank">
+              <Button icon={civitaiLogo} title="更多模型" />
+            </a>
+            <a href="https://svg.vedegpt.com" rel="noreferrer" target="_blank">
+              <Button icon={<BoldOutlined />} title="矢量化" />
+            </a>
+            <Button icon={<LinkOutlined />} onClick={showModal} title="VedeGPT" />
+            <Setting />
+            <Button icon={themeIcon[themeMode]} onClick={handleSetTheme} title="Switch Theme" />
+          </Space.Compact>
+        </HeaderView>
+      </DraggablePanel>
+      <Modal
+        footer={false}
+        onCancel={handleCancel}
+        open={isModalOpen}
+        title={
+          <a href="http://chat.vedegpt.com" rel="noreferrer" target="_blank">
+            <Space>
+              <LinkOutlined />
+              {'私人定制的ChatGPT'}
+            </Space>
+          </a>
+        }
+      >
+        {/* <Giscus themeMode={themeMode} /> */}
+      </Modal>
+    </>
+  );
 });
 
 export default Header;
